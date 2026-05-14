44df5e288414f2da6d79d77029dab906b020b6bd
Arnaldo Junior <abs.junnior@hotmail.com>
2026-05-14 11:53:00 -0300

    Enum OrderStatus

--- src/main/java/com/absjrdev/maxvendas/config/TestConfig.java
+++ src/main/java/com/absjrdev/maxvendas/config/TestConfig.java
@@ -1,6 +1,7 @@
 package com.absjrdev.maxvendas.config;
 
 import com.absjrdev.maxvendas.order.domain.Order;
+import com.absjrdev.maxvendas.order.domain.OrderStatus;
 import com.absjrdev.maxvendas.order.repository.OrderRepository;
 import com.absjrdev.maxvendas.user.domain.User;
 import com.absjrdev.maxvendas.user.repository.UserRepository;
@@ -27,8 +28,7 @@ class TestConfig implements CommandLineRunner {
     private OrderRepository orderRepository;
 
     @Override
-    public
-    void run(String... args) throws Exception {
+    public void run(String... args) throws Exception {
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com",
                 "988888888", "123456");
@@ -36,9 +36,9 @@ class TestConfig implements CommandLineRunner {
                 "977777777", "123456");
 
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/com/absjrdev/maxvendas/order/domain/Order.java
+++ src/main/java/com/absjrdev/maxvendas/order/domain/Order.java
@@ -2,7 +2,6 @@ package com.absjrdev.maxvendas.order.domain;
 
 import com.absjrdev.maxvendas.user.domain.User;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -21,60 +20,67 @@ class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
-//    @JsonIgnore
+    //Apenas para o banco
+    private Integer orderStatus;
+
+    //    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
-    public  Order() {
+    public Order() {
     }
-    public Order(Long id, Instant moment, User client) {
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
-    public
-    Long getId() {
+    public Long getId() {
         return id;
     }
 
-    public
-    void setId(Long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public
-    Instant getMoment() {
+    public Instant getMoment() {
         return moment;
     }
 
-    public
-    void setMoment(Instant moment) {
+    public void setMoment(Instant moment) {
         this.moment = moment;
     }
 
-    public
-    User getClient() {
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
+    public User getClient() {
         return client;
     }
 
-    public
-    void setClient(User client) {
+    public void setClient(User client) {
         this.client = client;
     }
 
     @Override
-    public
-    boolean equals(Object o) {
+    public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Order order = (Order) o;
         return Objects.equals(id, order.id);
     }
 
     @Override
-    public
-    int hashCode() {
+    public int hashCode() {
         return Objects.hashCode(id);
     }
 }
