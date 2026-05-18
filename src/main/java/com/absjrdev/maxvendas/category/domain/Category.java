cf8634913d59191cbdf6bbba5273d41aec09321c
Arnaldo Junior <abs.junnior@hotmail.com>
2026-05-18 17:48:04 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/absjrdev/maxvendas/category/domain/Category.java
+++ src/main/java/com/absjrdev/maxvendas/category/domain/Category.java
@@ -1,6 +1,7 @@
 package com.absjrdev.maxvendas.category.domain;
 
 import com.absjrdev.maxvendas.product.domain.Product;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -18,52 +19,47 @@ class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
+
     public Category(Long id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public
-    Long getId() {
+    public Long getId() {
         return id;
     }
 
-    public
-    void setId(Long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public
-    String getName() {
+    public String getName() {
         return name;
     }
 
-    public
-    void setName(String name) {
+    public void setName(String name) {
         this.name = name;
     }
 
-    public
-    Set<Product> getProducts() {
+    public Set<Product> getProducts() {
         return products;
     }
 
     @Override
-    public
-    boolean equals(Object o) {
+    public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Category category = (Category) o;
         return Objects.equals(id, category.id);
     }
 
     @Override
-    public
-    int hashCode() {
+    public int hashCode() {
         return Objects.hashCode(id);
     }
 }
